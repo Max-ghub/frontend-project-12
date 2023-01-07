ff1a7eb6b656fd136ad4f5a46e952da6ec71776a
Max <max@mail.ru>
2023-01-07 11:15:06 +0500

    refactor: auth form

--- front/src/components/AuthPage/AuthForm.jsx
+++ front/src/components/AuthPage/AuthForm.jsx
@@ -1,9 +1,9 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Formik, Form, Field } from 'formik';
+import { useFormik } from 'formik';
+import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 import * as yup from 'yup';
-import cn from 'classnames';
 // Contexts
 import { AuthContext } from '../../contexts/AuthContext';
 // Routes
@@ -15,64 +15,63 @@ const AuthForm = () => {
   const [authFailed, setAuthFailed] = useState(false);
 
   const authSchema = yup.object().shape({
-    username: yup.string().min(3).required(),
-    password: yup.string().min(3).required(),
+    username: yup.string().required(),
+    password: yup.string().required(),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.post(routes.loginPath(), values);
+        auth.login(response.data);
+        setAuthFailed(false);
+        navigate('/');
+      } catch (error) {
+        console.log(values);
+        setAuthFailed(true);
+      }
+    },
+    validationSchema: authSchema,
   });
 
   return (
-    <Formik
-      initialValues={{ username: '', password: '' }}
-      onSubmit={async (values) => {
-        try {
-          const response = await axios.post(routes.loginPath(), values);
-          auth.login(response.data);
-          setAuthFailed(false);
-          navigate('/');
-        } catch (error) {
-          setAuthFailed(true);
-        }
-      }}
-      validationSchema={authSchema}
-    >
-      {(props) => {
-        const fieldClasses = ['form-control', {
-          'is-invalid': authFailed && props.submitCount,
-        }];
-
-        return (
-          <Form className="col-12 col-md-6 mt-3 mt-mb-0">
-            <h1 className="text-center mb-4">Войти</h1>
-
-            <div className="form-floating mb-3">
-              <Field
-                className={cn(fieldClasses)}
-                id="username"
-                name="username"
-                autoComplete="username"
-                placeholder="Ваш ник"
-                required
-              />
-              <label htmlFor="username">Ваш ник</label>
-            </div>
-
-            <div className="form-floating mb-3">
-              <Field
-                className={cn(fieldClasses)}
-                id="password"
-                name="password"
-                autoComplete="current-password"
-                placeholder="Пароль"
-                required
-              />
-              <label htmlFor="username">Пароль</label>
-              {authFailed && props.submitCount ? <div className="invalid-tooltip">Неверные имя пользователя или пароль</div> : null}
-            </div>
-
-            <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
-          </Form>
-        );
-      }}
-    </Formik>
+    <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
+      <h1 className="text-center mb-4">Войти</h1>
+      <Form.Group className="form-floating mb-3">
+        <Form.Control
+          onChange={formik.handleChange}
+          value={formik.values.username}
+          isInvalid={authFailed}
+          id="username"
+          name="username"
+          autoComplete="username"
+          placeholder="Ваш ник"
+          required
+        />
+        <Form.Label htmlFor="username">Ваш ник</Form.Label>
+      </Form.Group>
+      <Form.Group className="form-floating mb-4">
+        <Form.Control
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          isInvalid={authFailed}
+          id="password"
+          name="password"
+          autoComplete="current-password"
+          placeholder="Пароль"
+          required
+        />
+        <Form.Label htmlFor="password">Пароль</Form.Label>
+        {authFailed && <Form.Control.Feedback type="invalid" tooltip>Неверные имя пользователя или пароль</Form.Control.Feedback>}
+      </Form.Group>
+      <Button type="submit" className="w-100 mb-3" variant="outline-primary" placement="right">
+        Войти
+      </Button>
+    </Form>
   );
 };
 
--- front/src/modals/AddChannel.jsx
+++ front/src/modals/AddChannel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import { Modal, Form, Button } from 'react-bootstrap';
