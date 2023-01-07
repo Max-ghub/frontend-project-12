4bf542dbe3f4da46e963c6b50a6fb4ef60815d43
Max <max@mail.ru>
2023-01-07 16:23:48 +0500

    feat: added logout button on chat page

--- front/src/components/Layout.jsx
+++ front/src/components/Layout.jsx
@@ -1,11 +1,18 @@
+import { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+// Contexts
+import { AuthContext } from '../contexts/AuthContext';
 
 const Layout = () => {
+  const auth = useContext(AuthContext);
+
   return (
     <>
       <nav className="shadow navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container">
           <Link className="navbar-brand text-light" to="/">Hexlet Chat</Link>
+          {auth.loggedIn() && <Button onClick={auth.logout}>Выйти</Button>}
         </div>
       </nav>
       <Outlet />
