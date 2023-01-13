6b1e318b9ba311ec923e35d4c526c21c944d5766
Max <max@mail.ru>
2023-01-13 14:29:29 +0500

    feat: customization NotFoundPage

--- frontend/src/components/Pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/components/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,16 @@
 /* eslint-disable import/prefer-default-export */
+import { Link } from 'react-router-dom';
+import notFoundImage from './notFoundPageImage.svg';
+
 const NotFoundPage = () => (
-  <h1>NotFound</h1>
+  <div className="text-center">
+    <img className="img-fluid h-25" src={notFoundImage} alt="Страница не найдена" />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      {'Но вы можете перейти '}
+      <Link to="/">на главную страницу</Link>
+    </p>
+  </div>
 );
 
 export { NotFoundPage };
